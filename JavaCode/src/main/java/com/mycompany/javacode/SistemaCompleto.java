f45081334c7b0b55b05c0dbbb37830191fb8ac1a
laecioandrade <laecio_a.rodrigues@outlook.com>
2019-08-20 21:27:30 -0300

    Arquivos distribuidos

---
+++ JavaCode/src/main/java/com/mycompany/javacode/SistemaCompleto.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.javacode;
+
+/**
+ *
+ * @author laeciorodrigues
+ */
+public class SistemaCompleto {
+    public static void main(String[] args) {
+        //TelaLogin tela = new TelaLogin();
+        //tela.setVisible(true);
+        TelaLogin tela = new TelaLogin();
+        tela.setVisible(true);
+    }
+}
